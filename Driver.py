1fadc9e025f98d24545d0d7d923f98b15d843a70
Ajay Melekamburath <ajaymk16@iisertvm.ac.in>
2022-02-17 17:11:16 +0530

    Added Author Info

--- Driver.py
+++ Driver.py
@@ -1,10 +1,22 @@
 # Dimer Generator by @ajay-mk
 # github.com/ajay-mk/
+# twitter.com/ajay_mk
+# This program is free software: you can redistribute it and/or modify it
+# If it is useful, I would appreciate an acknowledgment to the author
 
 # This files controls the workflow of the program
 
+import sys
 from InputProcessing import *
 from GenerateDimer import *
 from Rotation import *
 from Translation import *
 
+# Printing some information
+print("*** *** *** *** ***")
+print()
+print("Dimer Generator")
+print("by @ajay-mk")
+print("Visit: https://github.com/ajay-mk/Dimer-Generator")
+print()
+print("*** *** *** *** ***")
