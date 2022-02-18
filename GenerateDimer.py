24ed2f27abbe0285798d2de6d4a9ebdf39f10030
Ajay Melekamburath <ajaymk16@iisertvm.ac.in>
2022-02-18 15:00:13 +0530

    Removed Display Method

--- GenerateDimer.py
+++ GenerateDimer.py
@@ -3,7 +3,7 @@
 
 # This file contains the class and functions for generating the dimer geometry
 
-import numpy as np, pandas as pd
+import numpy as np
 
 class Dimer_Geom:
     """Class for generating the dimer geometry"""
@@ -20,12 +20,4 @@ class Dimer_Geom:
         self.coords = np.concatenate((self.monomer_geom, self.modified_monomer_geom), axis=0)
         self.atoms = np.concatenate((self.monomer_atoms, self.modified_monomer_atoms), axis=0)
         return self.coords, self.atoms
-    
-    def display_coordinates(self):
-        self.df = pd.DataFrame(self.coords, columns=['X', 'Y', 'Z'])
-        self.df.insert(0, 'Atom', self.atoms)
-        self.df = self.df.reset_index(drop=True)
-        print("Dimer Geometry \n")
-        print(self.df.to_string(index=False))
-        return self.df
     
